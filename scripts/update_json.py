5aae9f9739f61560339409e0b5df3ffa918584e9
cmf2201 <cmfrench808@gmail.com>
2025-07-28 16:54:40 -0400

    Fixed mistakes

--- scripts/update_json.py
+++ scripts/update_json.py
@@ -8,10 +8,10 @@ base_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'plates')
 output_folder = os.path.join(os.path.dirname(base_dir), 'json_files')
 
 # Update the JSON file with the latest license plate data
-generate_plate_json(base_dir,output_folder + '\\plate_names.json')
+generate_plate_json(base_dir,os.path.join(output_folder,'plate_names.json'))
 
-# Update the state page JSON file
-generate_state_page_json(base_dir, output_folder + '\\state_license_index.json')
+# # Update the state page JSON file
+generate_state_page_json(base_dir, os.path.join(output_folder,'state_license_index.json'))
 
-# Update the first image JSON file
-first_image(base_dir, output_folder + '\\first_image_index.json')
+# # Update the first image JSON file
+first_image(base_dir, os.path.join(output_folder,'first_image_index.json'))
